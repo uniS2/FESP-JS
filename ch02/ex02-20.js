fc7b8976c8afc5a1173c9550b182117076302c97
uniS2 <s2oie1010@gmail.com>
2023-10-26 08:20:54 +0900

    Homework. 231025 과제 제출

--- ch02/ex02-20.js
+++ ch02/ex02-20.js
@@ -24,7 +24,7 @@ var todolist = [
 // 완료된 할일 목록
 var doneList = [];
 for (let elem of todolist) {
-  if (elem['done']) {
+  if (elem.done) {
     doneList.push(elem);
   }
 }
@@ -33,28 +33,55 @@ console.log('완료된 할일 목록', doneList);
 // 남은 할일 목록
 var reaminList = [];
 for (let elem of todolist) {
-  if (!elem['done']) {
+  if (!elem.done) {
     reaminList.push(elem);
   }
 }
 console.log('남은 할일 목록', reaminList);
 
 // 남은 할일 수
-var reaminCount = reaminList.length;
+var reaminCount = 0;
+for (let elem of todolist) {
+  if (!elem.done) {
+    reaminCount++;
+  }
+}
 console.log('남은 할일 수', reaminCount);
 
 // _id=2인 할일
 var todo = {};
+for (let elem of todolist) {
+  if (elem._id === 2) {
+    todo = elem;
+  }
+}
 console.log('_id=2인 할일', todo);
 
 // _id=3인 할일의 index
 var todoIndex = 0;
+for (let i = 0; i < todolist.length; i++) {
+  if (todolist[i]._id === 3) {
+    todoIndex = i;
+  }
+}
 console.log('_id=3인 할일의 index', todoIndex);
 
 // 남은 할일이 하나라도 있는가?
 var hasTodo = false;
+for (let elem of todolist) {
+  if (!elem.done) {
+    hasTodo = true;
+    break;
+  }
+}
 console.log('남은 할일이 하나라도 있는가?', hasTodo);
 
 // 할일이 모두 완료 되었는가?
 var busy = true;
+for (let elem of todolist) {
+  if (!elem.done) {
+    busy = false;
+    break;
+  }
+}
 console.log('할일이 모두 완료 되었는가?', busy);
